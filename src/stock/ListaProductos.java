8bc11a73b15147fc60a552a8d974a085baf2f13a
beatboxgascon <32103615+beatboxgascon@users.noreply.github.com>
2018-02-06 18:34:21 +0100

    .

---
+++ src/stock/ListaProductos.java
@@ -0,0 +1,44 @@
+package stock;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author usuario
+ */
+public class ListaProductos {
+    
+    List<Producto> productos= new ArrayList<>();
+    
+    public void anadir(){
+        
+    }
+    
+    public ListaProductos buscar(String texto){
+        return null;
+    }
+    
+    public long getCantidad(){
+        return 1;
+    }
+    
+    public Producto getProducto(){
+        return null;
+    }
+    
+    public void guardar(){
+        
+    }
+    
+    public void cargar(){
+        
+    }
+    
+}
