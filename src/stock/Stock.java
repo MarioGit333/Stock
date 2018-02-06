c397127f264d29f132793e917e35c0d71778139c
MarioGit333 <32578491+MarioGit333@users.noreply.github.com>
2018-02-06 17:50:20 +0100

    .

--- src/stock/Stock.java
+++ src/stock/Stock.java
@@ -1,4 +1,3 @@
-
 package stock;
 
 import java.sql.Connection;
@@ -9,25 +8,36 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
+/*
+   nombre de la BBDD: controlStock
+
+    CREATE TABLE stock(
+            referencia VARCHAR(20) PRIMARY KEY,
+        nombre VARCHAR(45),
+        precioCompra NUMERIC(10,2),
+        precioVenta NUMERIC(10,2),
+            stock NUMERIC(10)
+    );
+
+ */
 public class Stock {
 
     
+    static Scanner entrada = new Scanner(System.in);
     public static void main(String[] args) {
         try {
-            Scanner entrada = new Scanner(System.in);
             String opcion = "";
-            
-            
+
             Class.forName("org.postgresql.Driver");
 
             String url = "jdbc:postgresql://localhost:5432/controlStock";
             String usuario = "postgres";
-            String password = "1234";
+            String password = "admin";
 
             Connection con = DriverManager.getConnection(url, usuario, password);
-            
+
             do {
-                System.out.println("1.AÃ±adir producto");
+                System.out.println("1.Añadir producto");
                 System.out.println("2.Buscar por nombre");
                 System.out.println("S.Salir");
                 opcion = entrada.nextLine();
@@ -38,10 +48,8 @@ public class Stock {
                         break;
                     case "3":
                         break;
-                    case "4":
-                        break;
                 }
-                
+
             } while (!opcion.equals("s"));
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(Stock.class.getName()).log(Level.SEVERE, null, ex);
@@ -50,6 +58,9 @@ public class Stock {
         }
     }
     
-    
-    
+    private static void anyadirProducto(){
+       
+        
+    } 
+
 }
